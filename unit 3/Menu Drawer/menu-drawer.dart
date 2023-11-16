73e8b206fd9968d4f904c7f117cc85982eab2499
Sumit Kumar <mail.sumitkumar30@gmail.com>
2023-11-16 11:37:05 +0530

    updated code for MenuDrawer

--- unit 3/Menu Drawer/menu-drawer.dart
+++ unit 3/Menu Drawer/menu-drawer.dart
@@ -1,3 +1,5 @@
+import 'package:first_app/main.dart';
+import 'package:first_app/register-screen.dart';
 import 'package:flutter/material.dart';
 
 class MenuDrawer extends StatelessWidget {
@@ -12,24 +14,42 @@ class MenuDrawer extends StatelessWidget {
           //   "Title",
           //   style: TextStyle(fontSize: 30),
           // )),
-          UserAccountsDrawerHeader(accountName: Text('John Doe', style: TextStyle(fontWeight: FontWeight.bold),), accountEmail: Text('johndoe@xyz.com'), currentAccountPicture: FlutterLogo(),),
+          UserAccountsDrawerHeader(
+            accountName: Text(
+              'John Doe',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            accountEmail: Text('johndoe@xyz.com'),
+            currentAccountPicture: FlutterLogo(),
+          ),
           ListTile(
             leading: Icon(Icons.home),
-            title: Text('Menu Item 1', style: TextStyle(fontSize: 20),),
+            title: Text(
+              'Menu Item 1',
+              style: TextStyle(fontSize: 20),
+            ),
             onTap: () {
-              Navigator.pop(context);
+             Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => MyApp()));
             },
           ),
           ListTile(
             leading: Icon(Icons.account_box),
-            title: Text('Menu Item 2', style: TextStyle(fontSize: 20),),
+            title: Text(
+              'Menu Item 2',
+              style: TextStyle(fontSize: 20),
+            ),
             onTap: () {
-              Navigator.pop(context);
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => RegisterScreen()));
             },
           ),
           ListTile(
             leading: Icon(Icons.logout),
-            title: Text('Menu Item 3', style: TextStyle(fontSize: 20),),
+            title: Text(
+              'Menu Item 3',
+              style: TextStyle(fontSize: 20),
+            ),
             onTap: () {
               Navigator.pop(context);
             },
